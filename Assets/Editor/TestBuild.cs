5bead3d39b894990120ea85991f5e2a72743bee0
김두원 <anah2run@com2us.com>
2023-12-07 14:33:51 +0900

    iOS 빌드 스크립트

--- Assets/Editor/TestBuild.cs
+++ Assets/Editor/TestBuild.cs
@@ -25,4 +25,25 @@ public class TestBuild
         // 빌드
         BuildPipeline.BuildPlayer(options);
     }
+
+    [MenuItem("Build/iOS")]
+    public static void PerformBuildiOS()
+    {
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        // 씬 추가
+        List<string> scenes = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            scenes.Add(scene.path);
+        }
+        options.scenes = scenes.ToArray();
+        // 타겟 경로(빌드 결과물이 여기 생성됨)
+        options.locationPathName = "Build/floatDouble/";
+        // 빌드 타겟
+        options.target = BuildTarget.iOS;
+
+        // 빌드
+        BuildPipeline.BuildPlayer(options);
+    }
 }
